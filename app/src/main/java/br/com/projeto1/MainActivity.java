435ec3259be49c808c2b6c048b7b403a2bf3c762
Evandro-Alves-Dev <70543731+Evandro-Alves-Dev@users.noreply.github.com>
2020-10-27 12:39:39 -0300

    Adicionado banco de dados e a função excluir no projeto.

---
+++ app/src/main/java/br/com/projeto1/MainActivity.java
@@ -0,0 +1,103 @@
+package br.com.projeto1;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import br.com.projeto1.database.ProdutoDAO;
+import br.com.projeto1.modelo.Produto;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listViewProdutos;
+    private ArrayAdapter<Produto> adapterProdutos;
+
+    private int id = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Produtos");
+        listViewProdutos = findViewById(R.id.ListView);
+        definirOnClickListenerListView();
+        definirOnLongClick();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ProdutoDAO produtoDAO = new ProdutoDAO(getBaseContext());
+        adapterProdutos = new ArrayAdapter<Produto>(MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                produtoDAO.listar());
+        listViewProdutos.setAdapter(adapterProdutos);
+
+    }
+
+    private void definirOnClickListenerListView() {
+        listViewProdutos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Produto produtoClicado = adapterProdutos.getItem(position);
+                Intent intent = new Intent(MainActivity.this, CadastroProdutoActivity.class);
+                intent.putExtra("produtoEditado", produtoClicado);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void definirOnLongClick() {
+        listViewProdutos.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                final Produto produtoClicado = adapterProdutos.getItem(position);
+
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.ic_delete)
+                        .setTitle("Excluir item?")
+                        .setMessage("Excluir permanentemente este item?")
+                        .setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                ProdutoDAO produtoDao = new ProdutoDAO(getBaseContext());
+                                boolean excluiu = produtoDao.excluir(produtoClicado);
+                                if (excluiu) {
+                                    onResume();
+                                    Toast.makeText(MainActivity.this, "Produto exluido com Sucesso.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        })
+                        .setNegativeButton("Não", null).show();
+                return true;
+            }
+        });
+    }
+
+    private ArrayList<Produto> criarListaProduto() {
+        ArrayList<Produto> produtos = new ArrayList<Produto>();
+        produtos.add(new Produto(0,"Impressora", 2580f));
+        produtos.add(new Produto(1,"Teclado", 450f));
+        produtos.add(new Produto(2,"Notebook", 4500f));
+        return produtos;
+    }
+
+    public void onClickNovoProduto(View v) {
+        Intent intent = new Intent(MainActivity.this, CadastroProdutoActivity.class);
+        startActivity(intent);
+
+    }
+}
